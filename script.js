678894ec1be7ce0db0792b5dc12b2b778b444d25
George Geils <95538913+ggeils@users.noreply.github.com>
2022-02-10 01:03:17 -0500

    added and pseudo'd script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Code time display on top of webpage
+
+// First function to initialize the blocks
+
+// Timer for time block color
+
+// Keep timeblocks updated in local storage
+
+// Write a second function that colors the time blocks
+
+// Save function for local storage
